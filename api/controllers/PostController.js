130ec161c2f90f385638930aa6b1c6ef213cc08c
Davy'd <pdaswift3009@gmail.com>
2018-01-16 23:56:36 +0100

    dashboard add form

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -13,6 +13,34 @@ module.exports = {
    //SHOW CONTROLLER
   show: function(req, res) {
     res.view('post/show')
+  },
+
+  //CREATE POST 
+  creat: function(req, res) {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+    var image = req.body.image;
+    
+    
+    if(body.length > 0 && title.length > 0){
+      Post.create(
+        {
+          title: title, 
+          body: body
+        })
+        .exec(function(err) {
+          if(err){
+            res.send(500,
+              {error: 'Could not create post'}
+            );
+          }
+          res.redirect('/post/home');
+        });
+    }else{
+         res.redirect('/admin/dashboard');
+    }
   }
+
 };
 
