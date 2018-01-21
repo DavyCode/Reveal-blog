32cbeeec4c26b83469f47a03e4d0f9c10bc3f04d
Davy'd <pdaswift3009@gmail.com>
2018-01-21 19:24:04 +0100

    refactor view

--- config/connections.js
+++ config/connections.js
@@ -18,7 +18,7 @@
  * For more information on configuration, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.connections.html
  */
-var db = require('./env/keys')
+// var db = require('./env/keys')
 
 module.exports.connections = {
 
